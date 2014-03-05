18337811c26f71ca4de1456765f4e0ccbede3e81
Jesse Wolgamott <jesse@comalproductions.com>
2014-03-04 20:26:23 -0500

    Update README to get started with Lineman

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# My Lineman Application
+# Google Maps Lite
+### Showcasing Promises in Angular
+
+------------------------
+
+### Get Started (after you clone)
+
+```
+lineman run
+```
+
+Then browse http://0.0.0.0:8000
+
+--------------------------
+
+### Resources
+
+0. http://docs.angularjs.org/api/ng/service/$q
+0. http://markdalgleish.com/2013/06/using-promises-in-angularjs-views/
+0. https://egghead.io/lessons/angularjs-chained-promises
+
+
+-------------------------------
+
+### License
+
+MIT (See LICENSE)
+
--- app/js/wayCoolCtrl.js
+++ app/js/wayCoolCtrl.js
@@ -1,8 +1,8 @@
-greenvilleJS.controller('wayCoolCtrl', ['$scope', '$q',  function ($scope, $q) {
+greenvilleJS.controller('wayCoolCtrl', ['$scope', '$q', function ($scope, $q) {
   $scope.address = "";
   $scope.mapMarkers= [];
   $scope.zoom = 8;
-  $scope.geoCoder = new google.maps.Geocoder(); 
+  $scope.geoCoder = new google.maps.Geocoder();
 
   $scope.mapCenter = function() {
     return $scope.map.center;
@@ -17,13 +17,13 @@ greenvilleJS.controller('wayCoolCtrl', ['$scope', '$q',  function ($scope, $q) {
   };
 
   $scope.initMap = function() {
-    $scope.currentPosition().then(function(center){
-      $scope.map.center = center;
+    $scope.currentPosition().then(function(newCoords){
+      $scope.map.center = newCoords;
     });
   };
 
   $scope.find = function() {
-    geoCode($scope.address).then(function(newCoords) {
+    geoCode($scope.address).then(function(newCoords){
       $scope.map.center = newCoords;
       $scope.mapMarkers = new Array(newCoords);
     });
@@ -32,8 +32,8 @@ greenvilleJS.controller('wayCoolCtrl', ['$scope', '$q',  function ($scope, $q) {
   geoCode = function(address) {
     var deferred = $q.defer();
     $scope.geoCoder.geocode({address: address}, function(results, status) {
-      deferred.resolve( { latitude:  results[0].geometry.location.d,
-                          longitude: results[0].geometry.location.e});
+      deferred.resolve({ latitude:  results[0].geometry.location.d,
+               longitude: results[0].geometry.location.e});
     });
     return deferred.promise;
   };
@@ -45,8 +45,5 @@ greenvilleJS.controller('wayCoolCtrl', ['$scope', '$q',  function ($scope, $q) {
     });
     return deferred.promise;
   };
-
-
 }
 ]);
-
